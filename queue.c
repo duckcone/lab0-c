83e3dc0fc02054a810e31a433f85b07b81ad5ae9
Ming-Ying Kuo <dylankuo0402@gmail.com>
2025-03-05 02:20:10 +0800

    Implement q_size()
    Change-Id: I68c6f761829a25c4f988db7a93aa1b503ab6125d

--- queue.c
+++ queue.c
@@ -122,7 +122,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head))
+        return 0;
+
+    struct list_head *tmp_node;
+    int len = 0;
+    list_for_each (tmp_node, head) {
+        len++;
+    }
+    return len;
 }
 
 /* Delete the middle node in queue */
